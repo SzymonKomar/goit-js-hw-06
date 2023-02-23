e954454e448edd9bcb754afd853303be3088ef15
SzymonKomar <114801537+SzymonKomar@users.noreply.github.com>
2023-02-23 15:08:16 +0100

    task 9 update
    Zmiania działania kodu

--- js/task-09.js
+++ js/task-09.js
@@ -1,10 +1,14 @@
 let buttonSelect = document.querySelector(".change-color");
 let spanSelect = document.querySelector(".color");
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 function bgcChange() {
-  let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  spanSelect.innerHTML = randomColor;
+  spanSelect.textContent = getRandomHexColor();
   let bodySelect = document.querySelector("body");
-  bodySelect.style.backgroundColor = randomColor;
+  bodySelect.style.backgroundColor = getRandomHexColor();
 }
 buttonSelect.addEventListener("click", bgcChange);
